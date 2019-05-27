177bab629cc0bd6b158faf7a437910a260ebcd1c
Dom Bennett <dominic.john.bennett@gmail.com>
2019-05-27 13:38:01 +0200

    adapted for restez

--- stages/1_phylotar.R
+++ stages/1_phylotar.R
@@ -1,11 +1,19 @@
 # Local functions ----
 source(file.path('tools', 'pipeline.R'))
 
+# Libs ----
+library(restez)
+library(phylotaR)
+
+# Restez setup ----
+restez_path_set('data')
+
 # Run ----
 outdir <- dir_gen('1_phylotar')
 # for linux
 ncbi_dr <- file.path("bin", "ncbi-blast-2.7.1+", "bin")
 # Hystricomorpha
 txid <- 33550
-phylotaR::setup(wd = outdir, txid = txid, ncbi_dr = ncbi_dr, overwrite = TRUE)
-phylotaR::run(wd = outdir)
+setup(wd = outdir, txid = txid, ncbi_dr = ncbi_dr, overwrite = TRUE)
+run(wd = outdir)
+#restart(wd = outdir)
