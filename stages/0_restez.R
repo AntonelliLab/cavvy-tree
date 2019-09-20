073494fa79e8279074c12133fc3fdd57700806cc
Dom Bennett <dominic.john.bennett@gmail.com>
2019-09-20 15:50:51 +0200

    updates

--- stages/0_restez.R
+++ stages/0_restez.R
@@ -1,5 +1,22 @@
-# Set up restez
+# Libs ----
 library(restez)
-restez_path_set(filepath = file.path('data'))
-db_download(preselection = 13)  # "Other mammalian"
-db_create()
+
+# Vars ----
+gb_section <- '16' # Rodents
+restez_path <- file.path('data', 'restez')
+
+# Set up ----
+if (!dir.exists(restez_path)) {
+  dir.create(restez_path)
+  restez_path_set(filepath = restez_path)
+  db_download(preselection = gb_section)
+  restez_connect()
+  db_create(min_length = 150)
+  restez_disconnect()
+}
+
+# Status check ----
+restez_path_set(filepath = restez_path)
+restez_connect()
+restez_status()
+restez_disconnect()
