6bca99f02005d110447ac9683134610b245e8da2
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-10-03 12:54:10 +0200

    updated

--- stages/1_phylotar.R
+++ stages/1_phylotar.R
@@ -1,4 +1,5 @@
 # Local functions ----
+dataflpth_get <- outdir_get <- NULL
 source(file.path('tools', 'pipeline.R'))
 
 # Libs ----
@@ -7,13 +8,20 @@ library(phylotaR)
 
 # Restez setup ----
 restez_path_set('data')
+restez_connect()
+if (!restez_ready()) {
+  stop('restez not ready.')
+}
+
+# Names ----
+cavvies_tbl <- read.csv(dataflpth_get('caviomorpha.csv'))
+ids <- cavvies_tbl[['Ids']]
 
 # Run ----
-outdir <- dir_gen('1_phylotar')
-# for linux
-ncbi_dr <- file.path("bin", "ncbi-blast-2.7.1+", "bin")
-# Hystricomorpha
-txid <- 33550
-setup(wd = outdir, txid = txid, ncbi_dr = ncbi_dr, overwrite = TRUE)
+outdir <- outdir_get('1_phylotar')
+setup(wd = outdir, txid = ids, overwrite = TRUE, outsider = TRUE, ncps = 2)
 run(wd = outdir)
 #restart(wd = outdir)
+
+# Clean-up ----
+restez_disconnect()
--- tools/pipeline.R
+++ tools/pipeline.R
@@ -1,6 +1,19 @@
-dir_gen <- function(script) {
-  # Make the results directory
-  flpth <- file.path('results', script)
+
+# dirs in stages/
+outdir_get <- function(nm) {
+  flpth <- file.path('stages', nm)
   if (!dir.exists(flpth)) dir.create(flpth)
+  flpth
+}
+indir_get <- function(nm) {
+  flpth <- file.path('stages', nm)
+  if (!dir.exists(flpth)) stop('No ', flpth, ' found.', call. = FALSE)
+  flpth
+}
+
+# filepaths in data/
+dataflpth_get <- function(nm) {
+  flpth <- file.path('data', nm)
+  if (!file.exists(flpth)) stop('No ', flpth, ' found.', call. = FALSE)
   flpth
 }
