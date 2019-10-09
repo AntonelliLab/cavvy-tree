966c27cee991e6619103372bdcb1e99596ed4381
Dom Bennett <dominic.john.bennett@gmail.com>
2019-10-09 17:07:59 +0200

    not db only

--- stages/1_phylotar.R
+++ stages/1_phylotar.R
@@ -21,7 +21,7 @@ ids <- cavvies_tbl[['Ids']]
 # Run ----
 outdir <- outdir_get('1_phylotar')
 setup(wd = outdir, txid = ids, overwrite = TRUE, outsider = TRUE, ncps = 2,
-      btchsz = 200, db_only = TRUE)
+      btchsz = 200, db_only = FALSE)
 run(wd = outdir)
 #restart(wd = outdir)
 
