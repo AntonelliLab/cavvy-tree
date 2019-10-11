5fe8f4ac1ae95439b0bc62d755057ae6517e1e33
Dominic Bennett <dominic.john.bennett@gmail.com>
2019-10-11 15:41:18 +0200

    less strict low supermatrix, wobble info

--- stages/4_supermatrix.R
+++ stages/4_supermatrix.R
@@ -1,22 +1,25 @@
-# TODO: add wobble base pair info
-
 # Local functions ----
 outdir_get <- indir_get <- NULL
 source(file.path('tools', 'pipeline.R'))
 
 # Functions ----
-partition_write <- function(nbps, flpth) {
+partition_write <- function(nbps, flpth, wobble = TRUE) {
   # write partition file in PartitionFinder format
   gene <- strt <- 1
   data_block <- NULL
   for (i in seq_along(nbps)) {
     end <- nbps[[i]] + strt - 1
-    data_block <- c(data_block, paste0('gene_', gene, "_pos1 = ", 
-                                       strt, "-", end, "\\3;"))
-    data_block <- c(data_block, paste0('gene_', gene, "_pos2 = ", 
-                                       strt + 1, "-", end, "\\3;"))
-    data_block <- c(data_block, paste0('gene_', gene, "_pos3 = ", 
-                                       strt + 2, "-", end, "\\3;"))
+    if (wobble) {
+      data_block <- c(data_block, paste0('gene_', gene, "_pos1 = ", 
+                                         strt, "-", end, "\\3;"))
+      data_block <- c(data_block, paste0('gene_', gene, "_pos2 = ", 
+                                         strt + 1, "-", end, "\\3;"))
+      data_block <- c(data_block, paste0('gene_', gene, "_pos3 = ", 
+                                         strt + 2, "-", end, "\\3;"))
+    } else {
+      data_block <- c(data_block, paste0('gene_', gene, " = ", 
+                                         strt, "-", end, ";"))
+    }
     strt <- end + 1
     gene <- gene + 1
   }
@@ -26,7 +29,7 @@ partition_write <- function(nbps, flpth) {
                pft[seq(from = i + 1, to = length(pft), by = 1)])
   writeLines(text = new_pft, flpth)
 }
-partition_finder_setup <- function(supermatrix, outdir, nm) {
+partition_finder_setup <- function(supermatrix, outdir, nm, wobble) {
   # setup a folder for PartitionFinder
   supermatrix <- supermatrix[[1]]
   nbps <- attr(supermatrix, 'nbps')
@@ -37,7 +40,8 @@ partition_finder_setup <- function(supermatrix, outdir, nm) {
   sequences_write(x = supermatrix, flpth = file.path(mtrx_dir,
                                                      'supermatrix.fasta'))
   partition_write(nbps = nbps, flpth = file.path(mtrx_dir,
-                                                 "partition_finder.cfg"))
+                                                 "partition_finder.cfg"),
+                  wobble = wobble)
 }
 
 # Library ----
@@ -74,24 +78,28 @@ alignment_list <- alignment_read(flpths = alignment_files)
 # column_cutoff - proportion of non-gaps per column
 # tip_cutoff - proportion of non-gaps per tip
 # three supermatrices:
-# low quality (70% gaps, all data)
+# low quality (80% gaps, all data)
 supermatrix <- supermatrices_get(alignment_list = alignment_list,
                                  min_ngenes = 2, min_ntips = 5, min_nbps = 200,
-                                 column_cutoff = 0.0, tip_cutoff = 0.1)
-partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'low')
+                                 column_cutoff = 0.0, tip_cutoff = 0.0)
+partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'low',
+                       wobble = TRUE)
 # mid quality (50% gaps, columns dropped)
 supermatrix <- supermatrices_get(alignment_list = alignment_list,
                                  min_ngenes = 2, min_ntips = 5, min_nbps = 200,
                                  column_cutoff = 0.1, tip_cutoff = 0.3)
-partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'mid')
+# Note: no wobble base pair if columns are dropped
+partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'mid',
+                       wobble = FALSE)
 # high quality (20% gaps, columns dropped)
 supermatrix <- supermatrices_get(alignment_list = alignment_list,
                                  min_ngenes = 2, min_ntips = 5, min_nbps = 200,
                                  column_cutoff = 0.2, tip_cutoff = 0.7)
-partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'high')
+partition_finder_setup(supermatrix = supermatrix, outdir = outdir, nm = 'high',
+                       wobble = FALSE)
 
 # PartitionFinder ----
-matrix_dirs <- file.path(outdir, c('low', 'mid', 'high'))
+matrix_dirs <- file.path(outdir, c('high', 'mid', 'low'))
 pfdir <- file.path(outdir, 'partitionfinder_results')
 if (!dir.exists(pfdir)) {
   dir.create(pfdir)
